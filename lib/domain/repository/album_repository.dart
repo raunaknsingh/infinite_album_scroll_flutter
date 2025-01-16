4abef0a4da95b5ca78eaa43774aa61390dee8f7a
raunak <raunak.singh@getir.com>
2025-01-16 22:32:08 +0530

    Add album repository interface contract

---
+++ lib/domain/repository/album_repository.dart
@@ -0,0 +1,8 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:navatech_infinite_album_scroll/domain/entity/album.dart';
+
+import '../../core/error/Failure.dart';
+
+abstract interface class AlbumRepository {
+  Future<Either<Failure, List<Album>>> fetchAlbums();
+}
